be96f6147c814ffd52233ef3300e696841c17d70
Praveen Manoharan <prmanoha@radisys.com>
2022-01-03 21:44:38 +0530

    fixed the issue

--- vars/clone_code.groovy
+++ vars/clone_code.groovy
@@ -1,6 +1,4 @@
 def call(Map config = [:]) {
-    sh '''
-	    echo "${config.manifestFile}"
-	    repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}
-		repo sync -j 11 '''
+    sh "repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}"
+	sh	''' repo sync -j 11 '''
 }
