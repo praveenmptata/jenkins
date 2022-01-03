e755f15e8bf56213426c6b8889d3fd4b42cb13ab
Praveen Manoharan <prmanoha@radisys.com>
2022-01-03 21:24:32 +0530

    fix for arg pass

--- vars/clone_code.groovy
+++ vars/clone_code.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
     sh '''
-	    repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}
+	    repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m $config.manifestFile
 		repo sync -j 11 '''
 }
