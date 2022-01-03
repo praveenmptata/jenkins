50ac6c0dae94081d31509de295815245ea898a79
Praveen Manoharan <prmanoha@radisys.com>
2022-01-03 22:11:37 +0530

    testing the error format

--- vars/clone_code.groovy
+++ vars/clone_code.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]) {
     sh "repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}"
 	sh	''' repo sync -j 11 '''
+	error "Testing the error"
 }
