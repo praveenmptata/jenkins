ab1881782d5202baeca60c08c2722fed9f435a21
Praveen Manoharan <prmanoha@radisys.com>
2022-01-03 23:27:51 +0530

    added default values

--- vars/clone_code.groovy
+++ vars/clone_code.groovy
@@ -1,11 +1,16 @@
-def call(Map config = [:]) {
-    sh '''git config --global user.name "Jenkins CI Group" 
-	      git config --global user.email "Jenkins.CIGroup@radisys.com" 
+def call(Map config = [thirdparty:false, clean_workSpace:true, optimize:false]) {
+    sh '''git config --global user.name "Jenkins CI Group"
+          git config --global user.email "Jenkins.CIGroup@radisys.com"
           git config --global credential.helper store '''
 	
 	if (config.clean_workSpace == true)
 	{
 	    sh ''' cd ${WORKSPACE}; rm -rf * '''
+	}
+	
+	if (config.thirdparty == true)
+	{
+	    checkout([$class: 'GitSCM', branches: [[name: '*/Arm_roadmap_jenkins_scripts']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '5g_jobs_thirdparty']], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '841769bd-6936-4c5e-aa77-5214885738e0', url: 'https://jenkins@blrgithub.radisys.com/scm/alm/lte/5g_jobs_thirdparty.git']]])
 	}
 	
 	if (config.optimize == true)
