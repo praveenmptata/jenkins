0a181b9b1f58e06658cf6d38ad821f9f9383db87
Praveen Manoharan <prmanoha@radisys.com>
2022-01-03 22:51:44 +0530

    trying new things like "if/else"

--- vars/clone_code.groovy
+++ vars/clone_code.groovy
@@ -1,5 +1,20 @@
 def call(Map config = [:]) {
-    sh "repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}"
-	sh	''' repo sync -j 11 '''
-	error "Testing the error"
+    sh '''git config --global user.name "Jenkins CI Group" 
+	      git config --global user.email "Jenkins.CIGroup@radisys.com" 
+          git config --global credential.helper store '''
+	
+	when ( equals (expected: true, actual: config.clean_workSpace))
+	{
+	    sh ''' cd ${WORKSPACE}; rm -rf * '''
+	}
+	
+	if (config.optimize == true)
+	{
+	    echo "optimization is enabled"
+	}
+	else
+	{
+        sh "repo init -u https://jenkins@blrgithub.radisys.com/scm/alm/lte/odsc_manifest.git -m ${config.manifestFile}"
+	    sh ''' repo sync -j 11 '''
+	}
 }
