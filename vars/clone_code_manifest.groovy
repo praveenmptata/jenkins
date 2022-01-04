aaa76cd2ee9886dd871f29610fe3b7530b431047
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 00:07:12 +0530

    trying new things

--- vars/clone_code_manifest.groovy
+++ vars/clone_code_manifest.groovy
@@ -1,15 +1,13 @@
-def call(Map Inputs = [thirdparty:true, clean_workSpace:true, optimize:false]) {
-
+def call(Map Inputs = [:].withDefault([thirdparty:true, clean_workSpace:true, optimize:false].&get)) {
+    
     println "Inputs : ${Inputs.toMapString()}"
 
     if (! Inputs.containsKey('manifestFile'))
-	{
-	    // TODO : Make sure Build status is marked as fail.
-		this.hello('FAILED')
-	    error("Manifest file not passes as input to clone_code step")
-	}
+    {
+        this.hello('FAILED')
+        error("Manifest file not passes as input to clone_code step")
+    }
 
-    // TODO : Make sure Build status is marked as In-Progress.
 	this.hello('INPROGRESS')
 	sh '''git config --global user.name "Jenkins CI Group"
           git config --global user.email "Jenkins.CIGroup@radisys.com"
@@ -38,11 +36,10 @@ def call(Map Inputs = [thirdparty:true, clean_workSpace:true, optimize:false]) {
 	
 }
 
-def hello(String Msg) {
-    echo "${Msg}"
-}
-
 def notifyStash(String state) {
+
+    checkout([$class: 'GitSCM', branches: [[name: '$Source_PR_Branch']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 120], [$class: 'CloneOption', noTags: true, reference: '', shallow: false, timeout: 120], [$class: 'WipeWorkspace'], [$class: 'RelativeTargetDirectory', relativeTargetDir: '5gran_radisys_odsc_dev']], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '841769bd-6936-4c5e-aa77-5214885738e0', url: 'https://jenkins@blrgithub.radisys.com/scm/alm/lte/products_cu.git']]])
+	
     if('SUCCESS' == state || 'FAILED' == state) {
         currentBuild.result = state         // Set result of currentBuild !Important!
     }
@@ -55,4 +52,9 @@ def notifyStash(String state) {
           projectKey: '',
           stashServerBaseUrl: 'https://alm.radisys.com/bitbucket'
 	])
+}
+
+
+def hello(String msg) {
+    echo "${msg}"
 }
