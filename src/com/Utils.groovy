fe63be458bc310b2ef599327c76b91a6babf4dba
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 16:33:14 +0530

    deleted unwanted code

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -4,19 +4,8 @@ package com
 import groovy.util.*
 import groovy.xml.*
 
-String changeSrcBranch(String filename, String integrationBranch, String srcBranch) {
-    printf('filename :  %s', filename)
-    printf('tgtBranch : %s', integrationBranch)
-    printf('srcBranch : %s', srcBranch)
-
-    def rootNode = new XmlSlurper().parse(filename)
-    println 'xml read is done'
-    def myNode = rootNode.depthFirst().find { it.name() == 'project' && it.@revision == integrationBranch}
-    println 'node is found'
-    myNode.@revision = srcBranch
-    println 'read is done'
-    new File(filename).withWriter {out-> XmlUtil.serialize(rootNode, out) }
-    return myNode.@path
+String changeSrcBranch() {
+    printf('place holder for utility functions')
 }
 
 return this
