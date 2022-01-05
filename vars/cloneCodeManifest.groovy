28ad7c0a918ed53afbf9eaaa35d8be841c4fd8fe
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 14:28:54 +0530

    Argument Fix

--- vars/cloneCodeManifest.groovy
+++ vars/cloneCodeManifest.groovy
@@ -44,7 +44,7 @@ def call(Map Inputs = [:] ) {
     if (para.containsKey('Source_PR_Branch')) {
         def manifestFilePath = "${WORKSPACE}/.repo/manifests/" +  "${Inputs.manifestFile}"
         println manifestFilePath
-        def checkoutPath = new Utils().changeSrcBranch(manifestFilePath, ${para.Dest_PR_Branch}, ${para.Source_PR_Branch})
+        def checkoutPath = new Utils().changeSrcBranch(manifestFilePath, "${para.Dest_PR_Branch}", "${para.Source_PR_Branch}")
         sh " cd ${WORKSPACE}/${checkoutPath}"
         sh " git branch; git log -n 1"
     }
