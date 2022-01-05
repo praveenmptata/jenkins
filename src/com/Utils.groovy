f8e98076ac0a57d86dc50c0ae7e48a0f590ea51c
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 15:37:14 +0530

    final debug

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -13,7 +13,9 @@ String changeSrcBranch(String filename, String integrationBranch, String srcBran
     def readxml = new File(filename)
     println 'File is able to open'
     def rootNode = new XmlSlurper().parse(readxml)
+    println 'xml read is done'
     def myNode = rootNode.depthFirst().find { it.name() == 'project' && it.@revision == integrationBranch}
+    println 'node is found'
     myNode.@revision = srcBranch
     println 'read is done'
     new File(filename).withWriter {out-> XmlUtil.serialize(rootNode, out) }
