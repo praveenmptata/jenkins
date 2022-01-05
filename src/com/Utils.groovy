c313d66916dea55784ed41e9e6e8769d51ccf3dd
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 14:30:07 +0530

    print fix in class

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -6,9 +6,9 @@ import groovy.util.*
 import groovy.xml.*
 
 String changeSrcBranch(String filename, String integrationBranch, String srcBranch) {
-    printf('filename : &s', filename)
-    printf('tgtBranch : &s', integrationBranch)
-    printf('srcBranch : &s', srcBranch)
+    printf('filename :  %s', filename)
+    printf('tgtBranch : %s', integrationBranch)
+    printf('srcBranch : %s', srcBranch)
 
     def rootNode = new XmlSlurper().parse(filename)
     def myNode = rootNode.depthFirst().find { it.name() == 'project' && it.@revision == integrationBranch}
