b9c62052b9777fc1606e5f89c88e9c5b5081abae
Praveen Manoharan <prmanoha@radisys.com>
2022-01-05 17:04:19 +0530

    Added the step to compile YOCTO CU OID

---
+++ vars/compileOdscYoctoCUOID.groovy
@@ -0,0 +1,29 @@
+
+def call(String workpacePath ) {
+
+    sh "cd ${workpacePath}"
+	sh '''
+	source env_setup
+	
+	source /root/sdk/environment-setup-aarch64-poky-linux
+	cd ${workpacePath}/5gran_jio_odsc/ngp/thirdparty/
+	cp -rf ${workpacePath}/5g_jobs_thirdparty/dpdk_YOCTO_JIO_ODSC.tar.gz .
+	tar -zxvf dpdk_YOCTO_JIO_ODSC.tar.gz
+
+	cd ${workpacePath}/5gran_jio_odsc/ngp/build
+	make TARGET=arm -j 20
+
+	cd ${workpacePath}/5gran_jio_odsc/5gran/cu/build/
+	./build_cu_arm.sh '''
+	
+
+    if (fileExists("${workpacePath}/5gran_jio_odsc/5gran/cu/build/cu_bin/bin/gnb_cu")) 
+    {
+        echo "***** gnb_cu binary is generated*****"
+    }
+	else
+	{
+		echo "gnb_cu is not generted"
+		exit 1
+	}
+}
