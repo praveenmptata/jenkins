d2e465b4da30d0cffda05710097fa899efeafdcb
Praveen Manoharan <prmanoha@radisys.com>
2022-01-06 23:19:52 +0530

    corrected print statement

--- vars/pipelineTeardown.groovy
+++ vars/pipelineTeardown.groovy
@@ -3,7 +3,7 @@ def call(Map Config = [:] ) {
     clenup = ['SUCCESS': true, 'FAILURE': true, 'ABORTED': true]
     Config = clenup + Config
     println "Inputs : ${Config.toMapString()}"
-    println "Build Status : currentBuild.currentResult"
+    println "Build Status : ${currentBuild.currentResult}"
 
     if (Config["${currentBuild.currentResult}"]) {
             echo 'cleaning up workspace'
