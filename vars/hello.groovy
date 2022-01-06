9a668818a632ea0b7f48354abbc80590b3fa8aad
Praveen Manoharan <prmanoha@radisys.com>
2022-01-06 13:30:54 +0530

    added testing code

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,4 +1,26 @@
 def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-	sh "exit 1"
+    sh "echo Hello"
+	def status_du_ut = sh(script : "cat du_ut.log | grep 'DU UT FAILED' | wc -l", returnStdout: true).trim()
+	def aval_reports = sh (script : "find . -name *REPORT.xml", returnStdout: true).trim()
+    print("du_ut :%s , aval : %s\n", status_du_ut, aval_reports)
+
+    if (status_du_ut.toInteger() > 0 ) {
+            echo 'du ut failed text found'
+    }
+
+	script {
+        if [ -f core* ]; then
+            echo "Core file detected"
+            ls -l core*
+            sshpass -p "zxcv@1234" ssh -o StrictHostKeyChecking=no root@172.26.0.132 "mkdir -p /root/10x/${BUILD_NUMBER}/"
+            sshpass -p "zxcv@1234" scp -r -o StrictHostKeyChecking=no core* root@172.26.0.132:/root/10x/${BUILD_NUMBER}/
+            sshpass -p "zxcv@1234" scp -r -o StrictHostKeyChecking=no gnb_du root@172.26.0.132:/root/10x/${BUILD_NUMBER}/
+            rm -rf core*
+            echo "core file copied to : 172.26.0.132"
+            echo "Path : /root/10x/${BUILD_NUMBER}/"
+            echo "Login details : root/zxcv@1234"
+        else
+            echo "All is well"
+        fi
+    }
 }
