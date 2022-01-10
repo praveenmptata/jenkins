89491efac6494735b7f42752eb16c892b7bda7c3
Praveen Manoharan <prmanoha@radisys.com>
2022-01-10 20:35:42 +0530

    edited the pipeline

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,11 +1,4 @@
-def call(Map Inputs = [:] ) {
-
-    // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-
+def call() {
     pipeline {
         agent { label "132_agent" }
 	    
