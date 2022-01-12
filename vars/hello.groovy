c619827666c3de2b0db053d9f92d355645ea90f5
Praveen Manoharan <prmanoha@radisys.com>
2022-01-13 02:07:04 +0530

    experiment 2
    Signed-off-by: Praveen Manoharan <prmanoha@radisys.com>

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,9 +2,13 @@ def call(String config = 'hello') {
     sh "echo ${config}"
 	dir('cool') {
 	    copyCodetoWs()
-		sh """#!/bin/bash
-		source env_setup
-		cd 5gran_jio_odsc/ngp/thirdparty/dpdk
-		cd 5gran_jio_odsc/5gran/cu/build/"""
+		sh '''#!/bin/bash
+		source env_setup'''
+		dir ('5gran_jio_odsc/ngp/thirdparty/dpdk') {
+		    sh 'pwd'
+		}
+		dir ('5gran_jio_odsc/5gran/cu/build/') {
+		    sh 'pwd'
+		}
 	}
 }
