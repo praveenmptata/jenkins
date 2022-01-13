f0965d4f9b7a26e72c0df50723c905876556c937
Praveen Manoharan <prmanoha@radisys.com>
2022-01-13 17:06:38 +0530

    added def

--- vars/myParallel.groovy
+++ vars/myParallel.groovy
@@ -1,9 +1,15 @@
 def call (Map pSteps = [:]) {
     m = [:]
-
+    defaultCfg = [statusFile: 'l2_odsc.txt']
+	pSteps = defaultCfg + pSteps
     sh " rm ${WORKSPACE}/l2_odsc.txt; touch ${WORKSPACE}/l2_odsc.txt"
 
     for( name in pSteps.keySet() ) {
+	
+		if (name in defaultCfg.keySet()) {
+		    continue
+		}
+		
 	    def myName = name
 		Closure body = pSteps[name]
         m[ myName ] = {
