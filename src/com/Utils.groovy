83deaeed03c69e8f6ffc69cea1da3c60f0c2b91c
Praveen Manoharan <prmanoha@radisys.com>
2022-01-15 17:30:36 +0530

    new job

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -1,6 +1,43 @@
 
 package com
 
-String sample(String cmd) {
-        println "place holder"
+/*** BEGIN META {
+  "name" : "reload job config",
+  "comment" : "Reload the job config of a specific job.",
+  "parameters" : [],
+  "core": "1.300",
+  "authors" : [
+    { name : "Thomas Froehlich - mail@thomas-froehlich.net" }
+  ]
+} END META**/
+
+
+import java.io.InputStream;
+import java.io.FileInputStream
+import java.io.File;
+import javax.xml.transform.stream.StreamSource
+
+
+String reloadJobConfig(String script, String toBeCopiedJobName, String folderName) {
+    def hudson = hudson.model.Hudson.instance;
+    def allJobs = hudson.model.Hudson.instance.getAllItems(Job.class).findAll { it.getFullName().contains(folderName) && it.name == toBeCopiedJobName} ;
+    if (allJobs.isEmpty()) {
+        println 'Error : No Job ${toBeCopiedJobName} found inside ${folderName}'
+		return false
+    }
+
+	for (job in allJobs) {
+	    if (jon.name == toBeCopiedJobName) {
+            def configXMLFile = job.getConfigFile();
+            def file = configXMLFile.getFile();
+            
+            InputStream is = new FileInputStream(file);
+            content = is.getText("UTF-8") 
+	        
+            job.updateByXml(new StreamSource(is));
+            job.save();         
+        }
+    }
+	
+	return true
 }
---
+++ vars/reloadJenkinScript.groovy
@@ -0,0 +1,10 @@
+import com.Utils
+
+def call(Map Inputs = [:] ) {
+    def scriptcontents = libraryResource 'pipeline.txt'
+	def jobName = "${config.jobName}" 
+	def foldername = "${config.folder}"
+	if (! new Utils(().reloadJobConfig(scriptcontents, jobName, foldername)) {
+	    sh 'exit 1'
+	}
+}
