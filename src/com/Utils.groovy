c40be43449d2a119e0e6a7fd29529567f44c1fe7
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 11:07:20 +0530

    try 5

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -28,7 +28,7 @@ boolean reloadJobConfig(String script, String toBeCopiedJobName, String folderNa
             def rootNode = new XmlSlurper().parse(file)
             def myNode = rootNode.depthFirst().find { it.name() == 'script'}
             myNode.text = script
-            def xml = new File(file)
+            def xml = new File(file.getAbsolutePath())
             xml.withWriter {out-> XmlUtil.serialize(rootNode, out) }
 
 			InputStream is = new FileInputStream(file)
