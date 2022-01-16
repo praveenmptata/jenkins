0a7b638eca9bbf97120bf49f728fcf535b7dc0c1
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 10:09:27 +0530

    syntax error

--- vars/reloadJenkinScript.groovy
+++ vars/reloadJenkinScript.groovy
@@ -4,7 +4,7 @@ def call(Map Inputs = [:] ) {
     def scriptcontents = libraryResource 'pipeline.txt'
 	def jobName = "${config.jobName}" 
 	def foldername = "${config.folder}"
-	if (! new Utils(().reloadJobConfig(scriptcontents, jobName, foldername)) {
+	if (! new Utils().reloadJobConfig(scriptcontents, jobName, foldername)) {
 	    sh 'exit 1'
 	}
 }
