cf7333e1e703d0157b1387ab8cb7f62cbda1f214
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 15:52:49 +0530

    try 1

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -8,25 +8,24 @@ import groovy.util.*
 import groovy.xml.*
 
 @NonCPS
-def getPipelineJobs(String jobname, String folderName) {
+def getPipelineJobs(String jobname) {
     def Ins = hudson.model.Hudson.instance
     def Jobs = Ins.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)
-    return Jobs.findAll { it.getFullName().contains(folderName) && it.name == jobname}
+    return Jobs.findAll { it.name == jobname}
 }
 
 
 @NonCPS
 def reloadJobConfig(String script, String jobname) {
-    /*
-    def allJobs = getPipelineJobs(jobname, folderName)
+
+    def allJobs = getPipelineJobs(jobname)
     if (allJobs.isEmpty()) {
         println "Error : No Job ${jobname} found inside ${folderName}"
         return false
     }
+    allJobs.each {println it.getFullName()}
 
-    for (job in allJobs) {
-	}*/
-	def job = hudson.model.Hudson.instance.getItem(jobname)
+	def job = hudson.model.Hudson.instance.getItemByFullName(jobname)
 	assert job != null && job instanceof org.jenkinsci.plugins.workflow.job.WorkflowJob : 'Input is not a pipeline job'
 
     if (job.getFullName() == jobname) {
