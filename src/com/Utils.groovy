74802f870eecbf55821e72be987c7e78eec83edf
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 10:13:03 +0530

    try 2

--- src/com/Utils.groovy
+++ src/com/Utils.groovy
@@ -1,17 +1,6 @@
 
 package com
 
-/*** BEGIN META {
-  "name" : "reload job config",
-  "comment" : "Reload the job config of a specific job.",
-  "parameters" : [],
-  "core": "1.300",
-  "authors" : [
-    { name : "Thomas Froehlich - mail@thomas-froehlich.net" }
-  ]
-} END META**/
-
-
 import java.io.InputStream;
 import java.io.FileInputStream
 import java.io.File;
@@ -19,8 +8,7 @@ import javax.xml.transform.stream.StreamSource
 import groovy.util.*
 import groovy.xml.*
 
-String reloadJobConfig(String script, String toBeCopiedJobName, String folderName) {
-    def hudson = hudson.model.Hudson.instance;
+boolean reloadJobConfig(String script, String toBeCopiedJobName, String folderName) {
     def allJobs = hudson.model.Hudson.instance.getAllItems(Job.class).findAll { it.getFullName().contains(folderName) && it.name == toBeCopiedJobName} 
     if (allJobs.isEmpty()) {
         println 'Error : No Job ${toBeCopiedJobName} found inside ${folderName}'
--- vars/reloadJenkinScript.groovy
+++ vars/reloadJenkinScript.groovy
@@ -2,6 +2,7 @@ import com.Utils
 
 def call(Map Inputs = [:] ) {
     def scriptcontents = libraryResource 'pipeline.txt'
+	echo "${scriptcontents}"
 	def jobName = "${config.jobName}" 
 	def foldername = "${config.folder}"
 	if (! new Utils().reloadJobConfig(scriptcontents, jobName, foldername)) {
