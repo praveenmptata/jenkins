14dce18bd46f38489c19bfca3625812fa44b6380
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 12:33:11 +0530

    job fail

--- vars/reloadJenkinScript.groovy
+++ vars/reloadJenkinScript.groovy
@@ -3,6 +3,7 @@ import com.Utils
 def call(Map Inputs = [:] ) {
     def scriptcontents = libraryResource 'pipeline.txt'
 	if (! new Utils().reloadJobConfig(scriptcontents, "${Inputs.jobName}" , "${Inputs.folder}")) {
-	    return 0
+	    println "Setting job as failed" 
+	    return 1
 	}
 }
