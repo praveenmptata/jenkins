dd637e83ec6e5e72b74da2504258eb17175f672e
Praveen Manoharan <prmanoha@radisys.com>
2022-01-16 12:28:55 +0530

    try 2

--- vars/reloadJenkinScript.groovy
+++ vars/reloadJenkinScript.groovy
@@ -2,7 +2,9 @@ import com.Utils
 
 def call(Map Inputs = [:] ) {
     def scriptcontents = libraryResource 'pipeline.txt'
-	if (! new Utils().reloadJobConfig(scriptcontents, "${Inputs.jobName}" , "${Inputs.folder}")) {
+	def ret = new Utils().reloadJobConfig(scriptcontents, "${Inputs.jobName}" , "${Inputs.folder}")
+	println ret
+	if (! ret) {
 	    sh 'exit 1'
 	}
 }
