121d47d69ac7306b3045c0fc52cac83a2b8f58e3
Praveen Manoharan <prmanoha@radisys.com>
2022-01-17 15:00:52 +0530

    try 1

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,14 +1,6 @@
-def call(String config = 'hello') {
-    sh "echo ${config}"
-	dir('cool') {
-	    copyCodetoWs()
-		sh '''#!/bin/bash
-		source env_setup'''
-		dir ('5gran_jio_odsc/ngp/thirdparty/dpdk') {
-		    sh 'pwd'
-		}
-		dir ('5gran_jio_odsc/5gran/cu/build/') {
-		    sh 'pwd'
-		}
-	}
+def call(String in, String config = 'hello', Map cfg = [:]) {
+    println config
+    println in
+	println "Inputs : ${cfg.toMapString()}"
+	
 }
