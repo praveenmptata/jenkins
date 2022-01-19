96a18d453ca9e56bc4c844efc8b66ee7145c23dc
Praveen Manoharan <prmanoha@radisys.com>
2022-01-19 22:10:19 +0530

    try 1

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,8 @@
 def call(String cfg, String inp = 'sample', String inp2 = 'sample1') {
-    println cfg
-	println "${inp}"
-	println "${inp2}"
+     def sample = false
+	 cool()
+}
+
+def cool() {
+    println "${sample}"
 }
