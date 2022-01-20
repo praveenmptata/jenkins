eee5ea4b77657d3df3d5b77e849029ebec690808
Praveen Manoharan <prmanoha@radisys.com>
2022-01-20 22:56:42 +0530

    try 4

--- vars/getCurentBranch.groovy
+++ vars/getCurentBranch.groovy
@@ -9,7 +9,7 @@ def call(String component) {
         sh 'exit 1'
     }
 
-    def branch = sh(script: "repo info ${repoMap[component.toLowerCase()]} | grep Current",
+    def branch = sh(script: "repo info ${repoMap[component.toLowerCase()]} | grep 'Manifest revision' | cut -d ':' -f 2",
                                                                         returnStdout: true).trim()
     println "${component} : ${branch}"
     return branch
