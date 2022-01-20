8f9c852a3246653009b7a1d67cd009ea60c7aa99
Praveen Manoharan <prmanoha@radisys.com>
2022-01-20 21:56:43 +0530

    commit

---
+++ vars/getBranch.groovy
@@ -0,0 +1,15 @@
+def call(String component) {
+    Map repoMap = [cu : "products_cu",
+                    du : "5gran_jio_odsc",
+                    platform : "platform_services"]
+
+    if(! repoMap.containsKey(component)) {
+        println "Invalid input for Function: ${component}"
+        sh 'exit 1'
+    }
+
+    def branch = sh(script: "repo info ${repoMap[component.toLowerCase()]} | grep 'Current branch' | cut -d ':' -f 2",
+                                                                        returnStdout: true).trim()
+    println "${component} : ${branch}"
+    return branch
+}
