6b28808525c84d427950e3177f2f446ffa5ef20c
Praveen Manoharan <prmanoha@radisys.com>
2022-02-16 23:46:58 +0530

    step

--- vars/reloadJenkinScript.groovy
+++ vars/reloadJenkinScript.groovy
@@ -1,9 +1,6 @@
 import com.Utils
 
-def call(Map Inputs = [:] ) {
-    def scriptcontents = libraryResource 'pipeline.txt'
-	if (! new Utils().reloadJobConfig(scriptcontents, "${Inputs.jobName}")) {
-	    println "Setting job as failed" 
-	    currentBuild.result = 'FAILURE'
-	}
+def call() {
+	def num = new Utils().getInstanceNumber()) 
+	println "executor: ${num}" 
 }
