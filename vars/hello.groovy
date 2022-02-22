8aea1e9fddbdc069e03ba69c634d7d75998ccde1
Praveen Manoharan <prmanoha@radisys.com>
2022-02-22 09:52:57 +0530

    try 5

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,8 @@
-@groovy.transform.Field
-def hello1 = sh(script: 'echo hello', returnStdout: true).trim()
-
 def call() {
-	println hello1
+    hello1 = sh(script: 'echo hello', returnStdout: true).trim()
+	print()
+}
+
+def print() {
+    println hello1
 }
