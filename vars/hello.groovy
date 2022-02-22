ae230f4613e82147ea2f230e478373bbd397977d
Praveen Manoharan <prmanoha@radisys.com>
2022-02-22 09:43:18 +0530

    try 1

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,6 @@
+@groovy.transform.Field
+def hello = sh script: 'echo hello', returnStdout: true).trim()
+
 def call() {
-    def y = 2
-    lock('ODSC_CU_UT_RES') {
-	    y = 1
-		sleep 5
-	}
-	println y
+	println hello
 }
