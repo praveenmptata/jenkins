86db5f4ef27439301ac47191ef9df804ad30c4d2
Praveen Manoharan <prmanoha@radisys.com>
2022-03-07 20:45:44 +0530

    try4

--- vars/constants_map.groovy
+++ vars/constants_map.groovy
@@ -1,4 +1,7 @@
 @groovy.transform.Field
-Map repo = [ (constants.manifest_file_9x)   : '9x',
-             (constants.manifest_file_10x)  : '10x']
+Map repo = [ (constants.manifest_9x)   : ['odsc_manifest.git', '5gran_jio_odsc', '9x'
+             (constants.manifest_10x)  : ['odsc_manifest.git', '5gran_jio_odsc', '10x'
+             (constants.manifest_ccdu) : ['ccdu_manifest.git', '5gran_jio_ccdu', 'ccdu'] ]
+			 .withDefault{ ['odsc_manifest.git', '5gran_jio_odsc', '9x'] }
+
 
