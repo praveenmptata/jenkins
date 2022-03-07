d97528c357126c1f78c3dd4f787d868ff8d403e7
Praveen Manoharan <prmanoha@radisys.com>
2022-03-07 20:50:35 +0530

    tru

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -3,3 +3,6 @@ def manifest_9x = "5gran_odsc_9.7_release_manifest.xml"
 
 @groovy.transform.Field
 def manifest_10x = "5gran_odsc_master_release_manifest.xml"
+
+@groovy.transform.Field
+def manifest_ccdu = "cool"
