2e0c2e56701677e304ae005d029fcfe89032a922
Praveen Manoharan <prmanoha@radisys.com>
2022-03-07 20:49:09 +0530

    try6

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,5 +1,5 @@
 @groovy.transform.Field
-def manifest_file_9x = "5gran_odsc_9.7_release_manifest.xml"
+def manifest_9x = "5gran_odsc_9.7_release_manifest.xml"
 
 @groovy.transform.Field
-def manifest_file_10x = "5gran_odsc_master_release_manifest.xml"
+def manifest_10x = "5gran_odsc_master_release_manifest.xml"
